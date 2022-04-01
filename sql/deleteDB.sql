a1aa1d5e7fbd09ed01de575d42e49b348217c60f
cgener3 <89759491+cgener3@users.noreply.github.com>
2022-03-31 22:38:05 -0500

    Add files via upload

---
+++ sql/deleteDB.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS city_sustainability.transportation;
+DROP TABLE IF EXISTS city_sustainability.carbon_emission_target;
+DROP TABLE IF EXISTS city_sustainability.energy_management;
+DROP TABLE IF EXISTS city_sustainability.waste_management;
+DROP TABLE IF EXISTS city_sustainability.company;
+DROP TABLE IF EXISTS city_sustainability.industry;
+DROP TABLE IF EXISTS city_sustainability.city;
+DROP TABLE IF EXISTS city_sustainability.country;
+DROP TABLE IF EXISTS city_sustainability.user;
+DROP TABLE IF EXISTS city_sustainability.entity1;
